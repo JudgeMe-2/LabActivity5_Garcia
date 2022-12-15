c61c311bd1dd9b7fabae0e3270dcead0db66f8dd
Jadge <joshuajadge.garcia@cit.edu>
2022-12-15 13:33:54 +0800

    Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -3,6 +3,7 @@ package LeapYear;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.InputMismatchException;
 
 public class LeapYearGUI extends JFrame {
     private JPanel panel1;
@@ -21,20 +22,26 @@ public class LeapYearGUI extends JFrame {
     }
 
     public void leapYearChecker() {
-        int year = Integer.parseInt(tfYear.getText());
+        int year = 0;
+        try {
+            year = Integer.parseInt(tfYear.getText());
+            if(year <= 0) {
+                throw new Exception("Not a Year");
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(panel1, "Input must be a number");
+            return;
+        } catch(Exception e) {
+            JOptionPane.showMessageDialog(panel1, "Input must be a year(nonnegative and nonzero)");
+            return;
+        }
         if (year % 4 == 0) {
-            // if the year is century
             if (year % 100 == 0) {
-                // if year is divided by 400
-                // then it is a leap year
                 if (year % 400 == 0)
                     JOptionPane.showMessageDialog(panel1, "Leap year");
                 else
                     JOptionPane.showMessageDialog(panel1, "Not a leap year");
-            }
-
-            // if the year is not century
-            else
+            } else
                 JOptionPane.showMessageDialog(panel1, "Leap year");
         } else {
             JOptionPane.showMessageDialog(panel1, "Not a leap year");
