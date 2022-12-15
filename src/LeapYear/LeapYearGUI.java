930e650ee24e736f9ea6666259344e602ffddb20
Jadge <joshuajadge.garcia@cit.edu>
2022-12-15 13:01:03 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,6 +1,8 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame {
     private JPanel panel1;
@@ -9,10 +11,40 @@ public class LeapYearGUI extends JFrame {
 
 
 
+    public LeapYearGUI() {
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                leapYearChecker();
+            }
+        });
+    }
+
+    public void leapYearChecker() {
+        int year = Integer.parseInt(tfYear.getText());
+        if (year % 4 == 0) {
+            // if the year is century
+            if (year % 100 == 0) {
+                // if year is divided by 400
+                // then it is a leap year
+                if (year % 400 == 0)
+                    JOptionPane.showMessageDialog(panel1, "Leap year");
+                else
+                    JOptionPane.showMessageDialog(panel1, "Not a leap year");
+            }
+
+            // if the year is not century
+            else
+                JOptionPane.showMessageDialog(panel1, "Leap year");
+        } else {
+            JOptionPane.showMessageDialog(panel1, "Not a leap year");
+        }
+    }
     public static void main(String[] args) {
         LeapYearGUI lp = new LeapYearGUI();
         lp.setContentPane(lp.panel1);
-        lp.setSize(200, 200);
+        lp.setTitle("Leap Year Checker");
+        lp.setSize(500, 300);
         lp.setDefaultCloseOperation(EXIT_ON_CLOSE);
         lp.setVisible(true);
     }
