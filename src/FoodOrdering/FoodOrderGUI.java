524f8af38ee91b847cb21ee9a6ea266e9d08183b
Jadge <joshuajadge.garcia@cit.edu>
2022-12-16 01:17:01 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,9 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FoodOrderGUI extends JFrame {
@@ -19,6 +22,49 @@ public class FoodOrderGUI extends JFrame {
     List<JCheckBox> foods;
     List<JRadioButton> discounts;
 
+
+    public FoodOrderGUI() {
+        foods = new ArrayList<>();
+        foods.add(cPizza);
+        foods.add(cBurger);
+        foods.add(cFries);
+        foods.add(cSoftDrinks);
+        foods.add(cTea);
+        foods.add(cSundae);
+        discounts = new ArrayList<>();
+        discounts.add(rbNone);
+        discounts.add(rb5);
+        discounts.add(rb10);
+        discounts.add(rb15);
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                order_price();
+            }
+        });
+    }
+    public void order_price() {
+        double total = 0;
+        int[] price = {100, 80, 65, 55, 50, 40};
+        double[] less = {0, .05, .10, .15};
+        int flag1 = 0, flag2 = 0;
+        for(int i = 0; i < price.length; i++) {
+            if(foods.get(i).isSelected()) {
+                total += price[i];
+                System.out.println(total);
+                flag1++;
+            }
+        }
+        for(int i = 0; i < less.length; i++) {
+            if(discounts.get(i).isSelected()) {
+                total -= total * less[i];
+                flag2++;
+                break;
+            }
+        }
+        JOptionPane.showMessageDialog(panel1, "The total price is Php " + String.format("%.2f", total));
+
+    }
     public static void main(String[] args) {
         FoodOrderGUI fo = new FoodOrderGUI();
         fo.setContentPane(fo.panel1);
