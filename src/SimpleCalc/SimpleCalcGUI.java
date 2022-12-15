b8786cdc1962078ef135b94e35c0fad9e918cbf8
Jadge <joshuajadge.garcia@cit.edu>
2022-12-15 16:10:37 +0800

    Complete Simplecalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,6 +1,8 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
@@ -10,6 +12,40 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
+    public SimpleCalcGUI() {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                compute();
+            }
+        });
+    }
+    public void compute() {
+        int num1 = Integer.parseInt(tfNumber1.getText());
+        int num2 = Integer.parseInt((tfNumber2.getText()));
+        int result;
+        String op = (String) cbOperations.getSelectedItem();
+
+        switch(op) {
+            case "+":
+                result = num1 + num2;
+                lblResult.setText(String.valueOf(result));
+                break;
+            case "-":
+                result = num1 - num2;
+                lblResult.setText(String.valueOf(result));
+                break;
+            case "/":
+                result = num1 / num2;
+                lblResult.setText(String.valueOf(result));
+                break;
+            case "*":
+                result = num1 * num2;
+                lblResult.setText(String.valueOf(result));
+                break;
+        }
+
+    }
     public static void main(String[] args) {
         SimpleCalcGUI sc = new SimpleCalcGUI();
         sc.setContentPane(sc.panel1);
